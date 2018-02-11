15256a1752ca9f075db6adde220bee14c679b8db
Rakib <rakibul.islam@bs-23.net>
2018-02-11 19:51:08 +0600

    Added periodic tasks

--- mycelery/myapp/tasks.py
+++ mycelery/myapp/tasks.py
@@ -3,6 +3,8 @@ import string
 from django.contrib.auth.models import User
 from django.utils.crypto import get_random_string
 from celery import shared_task, current_task
+from celery.schedules import crontab
+from celery.task import periodic_task
 
 
 @shared_task
@@ -16,3 +18,18 @@ def create_random_user_accounts(total_user):
                                   meta={'current': i, 'total': total_user,
                                         'percent': int((float(i) / total_user) * 100)})
     return {'current': total_user, 'total': total_user, 'percent': 100}
+
+
+@shared_task
+def add(a, b):
+    print(a + b)
+
+
+@shared_task
+def mul(a, b):
+    print(a * b)
+
+
+@periodic_task(run_every=(crontab(minute='*/1')), name="task-hello")
+def print_hello():
+    print('Hello World!')
